9203704b909116685df7f79f5a7fa5b778492010
crepppy <chapmanjack0777@gmail.com>
2021-01-04 23:50:21 +0000

    testimonial images now use gatsby-image to optimise loading times

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,25 +1,48 @@
 import React from 'react'
 import Flickity from 'react-flickity-component'
 import styles from './testimonials.module.scss'
-
-import emadeonImg from '../images/emadeon.jpg'
-import gcnImg from '../images/gcn.jpg'
-import manhuntImg from '../images/manhunt.png'
+import Img from 'gatsby-image'
+import {graphql, useStaticQuery} from "gatsby";
 
 export default function Testimonials() {
+    const profileImg = useStaticQuery(graphql`
+        fragment profileImg on File {
+            childImageSharp {
+                fluid(quality: 100, maxWidth: 328) {
+                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+            }
+        }
+
+        query {
+            emadeon: file(relativePath: {eq: "emadeon.jpg"}) {
+                ...profileImg
+            }
+
+            gcn: file(relativePath: {eq: "gcn.jpg"}) {
+                ...profileImg
+            }
+
+            manhunt: file(relativePath: {eq: "manhunt.png"}) {
+                ...profileImg
+            }
+        }
+    `)
     const flickityOptions = {
         wrapAround: true,
         pageDots: false,
         autoPlay: 5000,
     }
-    // TODO Convert images to use gatsby-image + experiment with auto play timing
+
     return (
         <section>
             <div className="container">
                 <h1>💬 Testimonials</h1>
                 <Flickity options={flickityOptions} static>
                     <div className={styles.carouselItem}>
-                        <img src={emadeonImg} alt=""/>
+                        <div className={styles.imgContainer}>
+                            <Img fluid={profileImg.emadeon.childImageSharp.fluid}/>
+                        </div>
                         <div>
                             <p>"crepppy always got the plugin done, he can code and configure almost any plugin. <span
                                 className={styles.underline}>He is an amazing developer</span>."</p>
@@ -27,7 +50,9 @@ export default function Testimonials() {
                         </div>
                     </div>
                     <div className={styles.carouselItem}>
-                        <img src={gcnImg} alt=""/>
+                        <div className={styles.imgContainer}>
+                            <Img fluid={profileImg.gcn.childImageSharp.fluid}/>
+                        </div>
                         <div>
                             <p>"<span className={styles.underline}>Fast, effective, and most of all quality</span>. You
                                 want something done the right way? crepppy is your guy."</p>
@@ -35,7 +60,9 @@ export default function Testimonials() {
                         </div>
                     </div>
                     <div className={styles.carouselItem}>
-                        <img src={manhuntImg} alt=""/>
+                        <div className={styles.imgContainer}>
+                            <Img fluid={profileImg.manhunt.childImageSharp.fluid}/>
+                        </div>
                         <div>
                             <p>"Jack was an absolute pleasure to work with, kept me in the loop the whole time and
                                 smashed out our plugins in a couple of days for an awesome price. <span
