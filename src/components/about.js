aec3110513fb8399df8fcc1f0c1275f72d121616
Jack Chapman <chapmanjack0777@gmail.com>
2022-01-23 19:57:25 +0000

    Change about.js description

--- src/components/about.js
+++ src/components/about.js
@@ -9,11 +9,12 @@ export default function About() {
                 <h2><span role="img" aria-label="person writing code on laptop emoji">👩🏽‍💻</span> About Me</h2>
                 <div className={styles.aboutContainer}>
                     <div className={styles.aboutContent}>
-                        <p>Hi, I’m Jack (formerly crepppy), a 16 year-old developer from the UK. I have been working
-                            with Java for 4 years as a freelancer offering a variety of services including Minecraft 
-                            plugins and Discord bots. For the past 2 years, I have also spent time working with clients 
-                            creating Python automation scripts and full-stack web applications.</p>
-                        <p>When you hire me, you can expect an active and experienced developer that can fulfil your 
+                        <p>Hi, I’m Jack, a 16 year-old developer from the UK. I have been working
+                            with Java for 5 years as a freelancer offering a variety of services including Minecraft
+                            plugins and Discord bots. I have also spent time working with clients
+                            creating automation scripts and full-stack web applications using languages such as Kotlin
+                            and Python.</p>
+                        <p>When you hire me, you can expect an active and experienced developer that can fulfil your
 	    		    request at a fair price with an efficient and organised approach.</p>
                     </div>
                     <img className={styles.aboutImage} src={codeSVG} width={500} height={375} alt="developer at a laptop programming"/>
