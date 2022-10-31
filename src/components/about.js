81b9f62b9f2c5b65a9dc54de05cca59d48074ec3
Jack Chapman <38158363+crepppy@users.noreply.github.com>
2022-10-31 16:00:12 +0000

    Update about wording

--- src/components/about.js
+++ src/components/about.js
@@ -9,12 +9,11 @@ export default function About() {
                 <h2><span role="img" aria-label="person writing code on laptop emoji">👩🏽‍💻</span> About Me</h2>
                 <div className={styles.aboutContainer}>
                     <div className={styles.aboutContent}>
-                        <p>Hi, I’m Jack, a 17 year-old developer from the UK. I have been working
-                            with Java for 5 years as a freelancer offering a variety of services including Minecraft
+                        <p>Hi, I’m Jack, a versatile software developer from Wales, UK. I have been working
+                            as a Java freelancer for 5 years, offering a variety of services including Minecraft
                             plugins and Discord bots. I have also spent time working with clients
-                            creating automation scripts and full-stack web applications using languages such as Kotlin
-                            and Python.</p>
-                        <p>When you hire me, you can expect an active and experienced developer that can fulfil your
+                            to create automation scripts and full-stack web applications using a variety of languages and technologies.</p>
+                        <p>When you hire me, you can expect an active and experienced developer that will fulfil your
 	    		    request at a fair price with an efficient and organised approach.</p>
                     </div>
                     <img className={styles.aboutImage} src={codeSVG} width={500} height={375} alt="developer at a laptop programming"/>
